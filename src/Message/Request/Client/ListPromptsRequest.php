abdbc664da1f8941b709984d9fa2e910603f545e
Maarten Paauw <github@paauw.dev>
2025-07-01 20:40:01 +0200

    fix(message): make list prompt paginated

--- src/Message/Request/Client/ListPromptsRequest.php
+++ src/Message/Request/Client/ListPromptsRequest.php
@@ -6,6 +6,7 @@ namespace Maartenpaauw\Mcp\Message\Request\Client;
 
 use Maartenpaauw\Mcp\JsonRpc;
 use Maartenpaauw\Mcp\Message\Request\Method;
+use Maartenpaauw\Mcp\Message\Request\PaginatedRequest;
 
 #[JsonRpc\Method(Method::ListPrompts)]
-final readonly class ListPromptsRequest implements Request {}
+final readonly class ListPromptsRequest extends PaginatedRequest implements Request {}
