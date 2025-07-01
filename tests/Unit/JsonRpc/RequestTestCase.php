785e1fa16005c6399deec5df83ee2f14c5d1a250
Maarten Paauw <github@paauw.dev>
2025-07-01 21:20:50 +0200

    test: rename complete request

--- tests/Unit/JsonRpc/RequestTestCase.php
+++ tests/Unit/JsonRpc/RequestTestCase.php
@@ -30,8 +30,8 @@ class RequestTestCase extends TestCase
         return [
             ['call_tool_request.php', 'call_tool_request.json'],
             ['call_tool_request_with_arguments.php', 'call_tool_request_with_arguments.json'],
-            ['complete_request.php', 'complete_request.json'],
-            ['complete_with_context_request.php', 'complete_with_context_request.json'],
+            ['complete_prompt_request.php', 'complete_prompt_request.json'],
+            ['complete_prompt_with_context_request.php', 'complete_prompt_with_context_request.json'],
             ['list_prompts_request.php', 'list_prompts_request.json'],
             ['list_prompts_with_cursor_request.php', 'list_prompts_with_cursor_request.json'],
             ['list_resource_templates_request.php', 'list_resource_templates_request.json'],
